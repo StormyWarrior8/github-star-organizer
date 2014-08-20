4b82ab116a69f671e653c95859e3a6b9447ce99e
Petr Balaban <balaban.petr@gmail.com>
2014-08-20 12:03:03 +0300

    Enable sidekiq monitor.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  #TODO: configure for production later
+  if Rails.env.development?
+    require 'sidekiq/web'
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   devise_for :users, only: [:omniauth_callbacks, :session], controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   root 'home#index'
